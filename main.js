6e93e1a5ab0fdcbb6eac5b1132ae44d0f2e9d293
imissh3r-tester <obitopines@gmail.com>
2025-10-05 23:37:35 +0700

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
             alert("Vui lòng điền đầy đủ thông tin."); 
             return;
         }
-        fetch('https://funny-sunshine-861602.netlify.app/api/submit', { // IP của thiết bị host server
+        fetch('http://192.168.55.102:3000/api/submit', { // IP của thiết bị host server
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -112,6 +112,7 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
 
 
 
